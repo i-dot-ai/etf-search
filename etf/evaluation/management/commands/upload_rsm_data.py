4a8edb020f4a9f8c5e2ee3cc00bcc4e9f4768223
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-06-09 17:12:57 +0100

    First pass at file upload

--- etf/evaluation/management/commands/upload_rsm_data.py
+++ etf/evaluation/management/commands/upload_rsm_data.py
@@ -1,14 +1,57 @@
+import csv
+import pathlib
+
+import httpx
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from etf.evaluation.management import utils
+DATA_DIR = settings.BASE_DIR / "temp-data"
+CHUNK_SIZE = 16 * 1024
+
+
+# Need maps for CSV headers to model property names
 
 
 class Command(BaseCommand):
     help = "Populate Evaluation Registry with data from RSM"
 
     def add_arguments(self, parser):
-        parser.add_argument("-f", "--filename", type=str, default="test.xlsx", help="Filename of data to upload")
+        parser.add_argument("-u", "--url", type=str, help="URL of data to upload")
 
     def handle(self, *args, **kwargs):
-        filename = kwargs["filename"]
-        utils.upload_all_data(filename)
+        url = kwargs["url"]
+        import_and_upload_evaluations(url)
+
+
+def save_url_to_data_dir(url):
+    filename = pathlib.Path(url).stem
+    filepath = DATA_DIR / "".join((filename, pathlib.Path(url).suffix))
+    if not filepath.exists():
+        print(f"Downloading to: {filepath}")  # noqa: T201
+        filepath.parent.mkdir(parents=True, exist_ok=True)
+        with filepath.open("wb") as f:
+            with httpx.stream("GET", url) as response:
+                for chunk in response.iter_bytes(CHUNK_SIZE):
+                    f.write(chunk)
+    else:
+        print(f"Skipping download: {filepath} already exists")  # noqa: T201
+    return filepath
+
+
+def get_sheet_headers(filename):
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        header = next(reader)
+    return header
+
+def get_evaluation_ids():
+
+def get_evaluation_rows_for_id():
+
+def transform_and_create_from_row():
+
+def import_and_upload_evaluations(url):
+    filename = save_url_to_data_dir(url)
+    headers = get_sheet_headers(filename)
+    print(headers)
+
