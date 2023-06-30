f89c1f8e198998ca57dfe3eb703b417018b0e252
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-06-30 17:42:56 +0100

    Added temporary heightened logging for test runs

--- etf/settings.py
+++ etf/settings.py
@@ -241,3 +241,19 @@ PUBLIC_PATHS = [
     "accounts/password-reset/",
     "accounts/change-password/reset/",
 ]
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
