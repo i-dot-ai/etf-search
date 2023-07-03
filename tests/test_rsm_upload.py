0fac78c626c5cbfa097b1fd002d00340f54c2990
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-07-03 15:24:16 +0100

    Finalised test, sorted and formatted

--- tests/test_rsm_upload.py
+++ tests/test_rsm_upload.py
@@ -1,5 +1,6 @@
 from etf.evaluation import models
-from etf.evaluation.management.commands.upload_rsm_data import transform_and_create_from_rows
+from etf.evaluation.management.commands.upload_rsm_data import \
+    transform_and_create_from_rows
 
 headers = [
     "Evaluation ID",
@@ -218,7 +219,7 @@ data_row = [
         "Other summary of methods",  # other summary of methods
         "Process description of analysis",  # process description of analysis
         "Superiority",  # impact analysis framework
-        "Impact analysis basis",  # impact analysis basis
+        "Intention-to-treat",  # impact analysis basis
         "Interviewed 300 businesses",  # impact analysis set
         "Relative measure",  # impact primary effect size measure type
         "Numerical outcome",  # impact primary effect size measure
@@ -236,14 +237,14 @@ data_row = [
         "Primary",  # primary of secondary outcome
         "Direct",  # direct or surrogate
         "Continuous",  # measure type
-        "average daily traffic counts",  # description of measure
+        "Average daily traffic counts",  # description of measure
         "Case study",  # collection procedures
         "feb",  # timepoint of interest month
         "2014",  # timepoint of interest year
         "Minimum difference",  # minimum practically important difference
         "Relevance of outcome",  # relevance of outcome
         "Another outcome",  # other outcome name
-        "DISCRETE",  # other outcome measure type
+        "Discrete",  # other outcome measure type
         "A small description",  # other outcome description of measure
         "Timing",  # other outcome collection procedures and timing
         "Process summary of findings",  # process summary of findings
@@ -268,7 +269,7 @@ data_row = [
         "Breaking confidentiality",  # breaking confidentiality
         "Other ethical info",  # other ethical information
         "Name of standard or process",  # name of standard or process
-        "Conformity",  # conformity
+        "Full",  # conformity
         "Process and standards description",  # process and standards description
     ],
 ]
@@ -278,7 +279,6 @@ def test_upload_evaluation():
     evaluations = models.Evaluation.objects.all()
     evaluations.delete()
     transform_and_create_from_rows(data_row, headers)
-    # print(data_row[0][headers.index("Impact - Analysis set")])
     evaluation = models.Evaluation.objects.first()
     assert evaluation.title == "Evaluation title", evaluation.title
     assert evaluation.short_title == "Short evaluation title", evaluation.short_title
@@ -297,7 +297,7 @@ def test_upload_evaluation():
     assert evaluation.evaluation_type_other is None, evaluation.evaluation_type_other
     assert evaluation.studied_population == "People.", evaluation.studied_population
     assert evaluation.eligibility_criteria == "Visitors to the area.", evaluation.eligibility_criteria
-    # assert evaluation.sample_size == 186, evaluation.sample_size  # Sample size not being set
+    assert evaluation.sample_size == 186, evaluation.sample_size
     assert evaluation.sample_size_units == "people", evaluation.sample_size_units
     assert evaluation.sample_size_details is None, evaluation.sample_size_details
     assert evaluation.process_for_recruitment == "Recruitment route.", evaluation.process_for_recruitment
@@ -314,7 +314,9 @@ def test_upload_evaluation():
     assert evaluation.participant_information == "Participant information.", evaluation.participant_information
     assert evaluation.participant_consent == "N", evaluation.participant_consent
     assert evaluation.participant_payment == "Participant payment.", evaluation.participant_payment
-    # assert evaluation.confidentiality_and_personal_data == "Confidentiality and personal info.", evaluation.confidentiality_and_personal_data  # Not setting
+    assert (
+        evaluation.confidentiality_and_personal_data == "Confidentiality and personal info."
+    ), evaluation.confidentiality_and_personal_data
     assert evaluation.breaking_confidentiality == "Breaking confidentiality.", evaluation.breaking_confidentiality
     assert evaluation.other_ethical_information == "Other ethical info.", evaluation.other_ethical_information
 
@@ -333,9 +335,9 @@ def test_upload_evaluation():
 
     assert evaluation.impact_framework == "SUPERIORITY", evaluation.impact_framework
     assert evaluation.impact_framework_other is None, evaluation.impact_framework_other
-    # assert evaluation.impact_basis == "Impact analysis basis.", evaluation.impact_basis  # not setting
+    assert evaluation.impact_basis == "INTENTION_TO_TREAT", evaluation.impact_basis
     assert evaluation.impact_basis_other is None, evaluation.impact_basis_other
-    # assert evaluation.impact_analysis_set == "interviewed 300 businesses.", evaluation.impact_analysis_set # not being set properly
+    assert evaluation.impact_analysis_set == "Interviewed 300 businesses.", evaluation.impact_analysis_set
     assert evaluation.impact_effect_measure_type == "RELATIVE", evaluation.impact_effect_measure_type
     assert evaluation.impact_effect_measure_type_other is None, evaluation.impact_effect_measure_type_other
     assert (
@@ -343,26 +345,36 @@ def test_upload_evaluation():
     ), evaluation.impact_primary_effect_size_measure
     assert evaluation.impact_effect_measure_interval == "CONFIDENCE", evaluation.impact_effect_measure_interval
     assert evaluation.impact_effect_measure_interval_other is None, evaluation.impact_effect_measure_interval_other
-    assert evaluation.impact_primary_effect_size_desc == "Primary effect measure description.", evaluation.impact_primary_effect_size_desc
+    assert (
+        evaluation.impact_primary_effect_size_desc == "Primary effect measure description."
+    ), evaluation.impact_primary_effect_size_desc
     assert evaluation.impact_interpretation_type == "INTERVALS", evaluation.impact_interpretation_type
     assert evaluation.impact_interpretation_type_other is None, evaluation.impact_interpretation_type_other
-    # assert evaluation.impact_sensitivity_analysis == "Impact sensitivity analysis.", evaluation.impact_sensitivity_analysis  # Not setting correctly
-    # assert evaluation.impact_subgroup_analysis == "Subgroup analysis.", evaluation.impact_subgroup_analysis  # Not setting
+    assert (
+        evaluation.impact_sensitivity_analysis == "Impact sensitivity analysis."
+    ), evaluation.impact_sensitivity_analysis
+    assert evaluation.impact_subgroup_analysis == "Subgroup analysis.", evaluation.impact_subgroup_analysis
     assert evaluation.impact_missing_data_handling == "Data handling.", evaluation.impact_missing_data_handling
     assert evaluation.impact_fidelity == "YES", evaluation.impact_fidelity
-    # assert evaluation.impact_description_planned_analysis == "Description of analysis.", evaluation.impact_description_planned_analysis  # Not setting correctly
+    assert (
+        evaluation.impact_description_planned_analysis == "Description of analysis."
+    ), evaluation.impact_description_planned_analysis
 
     assert evaluation.economic_type is None, evaluation.economic_type
     assert evaluation.perspective_costs == "A lot of costs that are included.", evaluation.perspective_costs
-    assert evaluation.perspective_benefits == "Some benefits included.", evaluation.perspective_benefits  # not being set
+    assert evaluation.perspective_benefits == "Some benefits included.", evaluation.perspective_benefits
     assert evaluation.monetisation_approaches == "Economic monetisation approach.", evaluation.monetisation_approaches
     assert evaluation.economic_design_details == "Economic evaluation design.", evaluation.economic_design_details
 
-    # assert evaluation.economic_analysis_description == "Economic description of analysis.", evaluation.economic_analysis_description  # not setting
+    assert (
+        evaluation.economic_analysis_description == "Economic description of analysis."
+    ), evaluation.economic_analysis_description
 
     assert evaluation.other_design_type == "Other evaluation design.", evaluation.other_design_type
 
-    # assert evaluation.other_analysis_description == "Other description of analysis.", evaluation.other_analysis_description
+    assert (
+        evaluation.other_analysis_description == "Other description of analysis."
+    ), evaluation.other_analysis_description
 
     assert evaluation.impact_comparison is None, evaluation.impact_comparison
     assert evaluation.impact_outcome is None, evaluation.impact_outcome
@@ -378,4 +390,68 @@ def test_upload_evaluation():
     assert evaluation.other_summary_findings == "Other summary of findings.", evaluation.other_summary_findings
     assert evaluation.other_findings == "Other findings.", evaluation.other_findings
 
-    assert False
+    assert evaluation.interventions.all().count() == 1
+    evaluation_intervention = evaluation.interventions.first()
+    assert evaluation_intervention.name == "Local Sustainable Transport Fund", evaluation_intervention.name
+    assert (
+        evaluation_intervention.brief_description == "Intervention brief description."
+    ), evaluation_intervention.brief_description
+    assert evaluation_intervention.rationale == "Rationale for the intervention.", evaluation_intervention.rationale
+    assert evaluation_intervention.materials_used == "Materials used.", evaluation_intervention.materials_used
+    assert (
+        evaluation_intervention.procedures == "Procedures used for the intervention."
+    ), evaluation_intervention.procedures
+    assert (
+        evaluation_intervention.provider_description == "The person who delivered it."
+    ), evaluation_intervention.provider_description
+    assert (
+        evaluation_intervention.modes_of_delivery == "How was it delivered."
+    ), evaluation_intervention.modes_of_delivery
+    assert evaluation_intervention.location == "Where the intervention was delivered.", evaluation_intervention.location
+    assert (
+        evaluation_intervention.frequency_of_delivery == "How often the intervention was delivered."
+    ), evaluation_intervention.frequency_of_delivery
+    assert evaluation_intervention.tailoring == "Tailoring.", evaluation_intervention.tailoring
+    assert evaluation_intervention.fidelity == "How well was it delivered.", evaluation_intervention.fidelity
+    assert (
+        evaluation_intervention.resource_requirements == "Resource requirements."
+    ), evaluation_intervention.resource_requirements
+    assert (
+        evaluation_intervention.geographical_information == "England."
+    ), evaluation_intervention.geographical_information
+
+    assert evaluation.outcome_measures.all().count() == 1
+    outcome_measure = evaluation.outcome_measures.first()
+    assert outcome_measure.name == "Reduced congestion", outcome_measure.name
+    assert outcome_measure.primary_or_secondary == "PRIMARY", outcome_measure.primary_or_secondary
+    assert outcome_measure.direct_or_surrogate == "DIRECT", outcome_measure.direct_or_surrogate
+    assert outcome_measure.measure_type == "CONTINUOUS", outcome_measure.measure_type
+    assert outcome_measure.measure_type_other is None, outcome_measure.measure_type_other
+    assert outcome_measure.description == "Average daily traffic counts", outcome_measure.description
+    assert outcome_measure.collection_process == "Case study", outcome_measure.collection_process
+    assert outcome_measure.timepoint is None, outcome_measure.timepoint
+    assert outcome_measure.minimum_difference == "Minimum difference", outcome_measure.minimum_difference
+    assert outcome_measure.relevance == "Relevance of outcome", outcome_measure.relevance
+
+    assert evaluation.other_measures.all().count() == 1
+    other_measure = evaluation.other_measures.first()
+    assert other_measure.name == "Another outcome", other_measure.name
+    assert other_measure.measure_type == "DISCRETE", other_measure.measure_type
+    assert other_measure.measure_type_other is None, other_measure.measure_type_other
+    assert other_measure.description == "A small description", other_measure.description
+    assert other_measure.collection_process == "Timing", other_measure.collection_process
+
+    assert evaluation.link_other_services.all().count() == 1
+    link = evaluation.link_other_services.first()
+    assert link.name_of_service == "https://www.google.com/", link.name_of_service
+    assert link.link_or_identifier == "https://www.google.com/", link.link_or_identifier
+
+    assert evaluation.process_standards.all().count() == 1
+    process_standard = evaluation.process_standards.first()
+    assert process_standard.name == "Name of standard or process.", process_standard.name
+    assert process_standard.conformity == "FULL", process_standard.conformity
+    assert process_standard.description == "Process and standards description.", process_standard.description
+
+    assert evaluation.costs.all().count() == 1
+    cost = evaluation.costs.first()
+    assert cost.item_cost == 1000, cost.item_cost
