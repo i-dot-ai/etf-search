a48c03f1082e0239815d8d2bfd87d00255aeeb00
Ed Singleton <singletoned@gmail.com>
2023-07-07 15:38:29 +0100

    Remove output dir

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -3,8 +3,5 @@
 set -o errexit
 set -o nounset
 
-# Copy across generated files to local docker-compose volume
-cp -R /output/* /app/static/dist/
-
 python manage.py migrate --noinput
 watchmedo auto-restart --directory=./  --pattern=""*.py"" --recursive -- waitress-serve --port=$PORT --threads=8 etf.wsgi:application
