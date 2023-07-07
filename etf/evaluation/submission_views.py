3130879f756bbd2a4066c16c36251b680b231980
Ed Singleton <singletoned@gmail.com>
2023-07-07 16:00:40 +0100

    Fix default value

--- etf/evaluation/submission_views.py
+++ etf/evaluation/submission_views.py
@@ -979,7 +979,7 @@ def evaluation_process_design_aspects_view(request, evaluation_id):
         except marshmallow.exceptions.ValidationError as err:
             errors = dict(err.messages)
         else:
-            aspect_name = serialized_aspects.get("aspect_name")
+            aspect_name = serialized_aspects.get("aspect_name", ())
             aspect_name_other = serialized_aspects.get("aspect_name_other")
             for aspect in aspect_name:
                 if aspect == choices.ProcessEvaluationAspects.OTHER.value:
